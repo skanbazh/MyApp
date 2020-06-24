18082ec868231a4d4531691bfb73e7adcbaa6224
skanbazh <santhosh8684@gmail.com>
2020-06-24 11:46:28 +0530

    MyApp first commit

---
+++ MyApp/src/main/java/com/ofss/MyApp/App.java
@@ -0,0 +1,14 @@
+package com.ofss.MyApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Calculator c = new Calculator();
+    	System.out.println( c.add(10, 20) );
+    }
+}
---
+++ MyApp/src/main/java/com/ofss/MyApp/Calculator.java
@@ -0,0 +1,12 @@
+package com.ofss.MyApp;
+
+public class Calculator {
+public int add(int a, int b)
+{
+	return a + b;
+}
+public int mul(int a, int b)
+{
+	return a * b;
+}
+}
